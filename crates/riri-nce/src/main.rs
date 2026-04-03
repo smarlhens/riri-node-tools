442dac4b3b8070c4e31a202d4d8e60cb04aade75
Samuel MARLHENS <samuel.marlhens@gmail.com>
2026-04-03 21:55:38 +0200

    refactor: move check-engines into riri-nce

--- crates/riri-nce/src/main.rs
+++ crates/riri-nce/src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+mod finder;
+
+use riri_npm::{parse_lock, parse_package};
+
+fn main() {
+    let package =
+        finder::get_package().expect("Unable to get package.json file in the current directory");
+    let package_lock = finder::get_most_recently_modified_lock()
+        .expect("Unable to get the most recently modified lock file in the current directory");
+    let (parsed_package, _, _) =
+        parse_package(&package).expect("Unable to parse package.json file");
+
+    println!("Package content: {parsed_package:?}");
+
+    let parsed_lock_package = parse_lock(&package_lock).expect("Unable to parse lock file");
+
+    println!("Lock content: {parsed_lock_package:?}");
+}
